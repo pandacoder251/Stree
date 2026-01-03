3919441c3c9a929f0c1d0ebc854c59b0aa17a608
Pousali <pousalidolai59@gmail.com>
2026-01-03 13:37:55 +0000

    add

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StreeHome from '../frontend/home'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StreeHome />
+  </React.StrictMode>,
+)
