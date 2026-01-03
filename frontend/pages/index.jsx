0b87a6f8dd426eb6699db44925ddb0dce594f97e
Pousali <pousalidolai59@gmail.com>
2026-01-03 19:09:56 +0000

    fresh commit

---
+++ frontend/pages/index.jsx
@@ -0,0 +1,6 @@
+// Entry point for React app
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './Home.jsx';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
